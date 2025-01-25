fb7e9569460ab24d65282ee0795df103d94b8159
sethdaking <lastemail768@gmail.com>
2025-01-25 16:23:12 +0200

    Turned off the eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,25 +5,7 @@ module.exports = {
   plugins: ["@typescript-eslint"],
   extends: ["eslint:recommended", "next/core-web-vitals", "prettier"],
   rules: {
-    "import/order": [
-      "error",
-      {
-        groups: ["builtin", "external", "internal", ["parent", "sibling"]],
-        pathGroups: [
-          {
-            pattern: "react",
-            group: "external",
-            position: "before",
-          },
-        ],
-        pathGroupsExcludedImportTypes: ["react"],
-        "newlines-between": "always",
-        alphabetize: {
-          order: "asc",
-          caseInsensitive: true,
-        },
-      },
-    ],
+    "import/order": "off",  // Disable the import order rule
   },
   overrides: [
     {
@@ -34,11 +16,11 @@ module.exports = {
       },
       extends: [
         "plugin:@typescript-eslint/recommended",
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking", 
 
-        //declaring 'next/core-web-vitals' and 'prettier' again in case
-        //the two plugin:... configs above overrode any of their rules
-        //Also, 'prettier' needs to be last in any extends array
+        // Declaring 'next/core-web-vitals' and 'prettier' again in case
+        // the two plugin:... configs above overrode any of their rules
+        // Also, 'prettier' needs to be last in any extends array
         "next/core-web-vitals",
         "prettier",
       ],
